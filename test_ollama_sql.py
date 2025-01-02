28a20ed8f14d3a789b9b1f727584c2075f2714a3
hank.aungkyaw <hank.lo.kyaw@gmail.com>
2025-01-01 21:36:03 -0800

    added ollama-sql query script.

---
+++ test_ollama_sql.py
@@ -0,0 +1,43 @@
+import ollama
+
+res = ollama.chat(
+    model="llama3.2",
+    messages=[
+        {
+            "role": "user",
+            "content": "why is the ocean so salty?",
+        },
+    ],
+    stream=False,
+)
+# for chunk in res:
+#     print(chunk["message"]["content"], end="", flush=True)
+
+print(res["message"]["content"])
+
+
+# ==================================================================================
+# ==== The Ollama Python library's API is designed around the Ollama REST API ====
+# ==================================================================================
+
+# # == Generate example ==
+# res = ollama.generate(
+#     model="llama3.2",
+#     prompt="why is the sky blue?",
+# )
+
+# show
+# print(ollama.show("llama3.2"))
+
+
+# # Create a new model with modelfile
+# modelfile = """
+# FROM llama3.2
+# SYSTEM You are very smart assistant who knows everything about oceans. You are very succinct and informative.
+# PARAMETER temperature 0.1
+# """
+#
+# ollama.create(model="knowitall", modelfile=modelfile)
+#
+# res = ollama.generate(model="knowitall", prompt="why is the ocean so salty?")
+# print(res["response"])
